b8e1490c262e05a8adaad9455cc4046b29624d2e
zhangtianyu <tianyu.zhang@tarsocial.com>
2021-12-10 18:34:10 +0800

    2021-12-10 增加fflogs配置文件

---
+++ utils/fflogs/getRanking.go
@@ -0,0 +1,13 @@
+package fflogs
+
+// V2 v2接口参数 目前不会graphQL 暂时搁置
+var V2 = map[string]string{
+	"ClientId":     "9513e857-07ee-4130-88f7-18857a58d291",
+	"ClientSecret": "arIyugOHwyHJcgsymLjjvpz6k4R7iUWNo1QdzfFH",
+	"OpenApi":      "https://www.fflogs.com/api/v2/client",
+}
+
+// V1 v1版本接口key
+var V1 = map[string]string{
+	"Key": "ad47257a0254969cd179b849624920f7",
+}
