8d81d7f2b61ef097ee63037992f8acfbdfdcaff8
MinakawaYuki <tianyu.zhang@tarsocial.com>
2022-01-19 18:34:37 +0800

    2021-01-19 自己用的功能

--- router/router.go
+++ router/router.go
@@ -3,11 +3,14 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"go-bot/controller"
+	"go-bot/utils/tools"
+	"net/http"
 )
 
 func InitRouter() {
 	r := gin.Default()
 	gin.SetMode(gin.DebugMode)
+	r.StaticFS("/image", http.Dir(tools.GetImageFullPath()))
 	api := r.Group("/api")
 	{
 		api.GET("/event", controller.GetEventMessage)
